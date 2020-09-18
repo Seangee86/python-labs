2440b84fbb52526c8cf9fdf5f73f2cc96c856ffa
Sean Granier <seanlg624@gmail.com>
2020-09-18 13:29:41 -0500

    01_01_run_it.py

--- 01_python_fundamentals/01_01_run_it.py
+++ 01_python_fundamentals/01_01_run_it.py
@@ -16,3 +16,15 @@
 '''
 print("hello world!")
 
+'''
+ There's 365 days in one year. 60 mins in one hour. 60 seconds in one minute. 
+ 
+60 seconds in one minute. 60sec * 60min = 3,600 seconds. So there's 3,600 seconds in 1 hour. 
+
+24 hours in 1 day 
+
+3,600sec * 24hours = 86,400 seconds in one day 
+
+86,400sec  * 365days  = 31,536,000 seconds in one year. Not counting leap year
+
+'''
