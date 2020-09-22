bc42c6fa4b6319be60ed5a52f542fcd21bd3240d
Sean Granier <seanlg624@gmail.com>
2020-09-22 14:53:04 -0500

    02_04 complete

--- 02_basic_datatypes/1_numbers/02_04_temp.py
+++ 02_basic_datatypes/1_numbers/02_04_temp.py
@@ -7,6 +7,13 @@ then convert it to Celsius and print it to the console.
     C = (F - 32) * (5 / 9)
 
 Output should read like - "81.32 degrees fahrenheit = 27.4 degrees celsius"
-
-
 '''
+
+fh = int(float(input("What is the temp in fahrenheit?")))
+
+cels = (fh - 32) * (5 / 9)
+
+print(f"{cels} degrees celsius")
+
+
+
