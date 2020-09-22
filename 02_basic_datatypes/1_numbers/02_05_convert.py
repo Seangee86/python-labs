7df5ca91ca83f4e3d1d29ceeeb6635d7243b05b0
Sean Granier <seanlg624@gmail.com>
2020-09-22 15:15:39 -0500

    02_05 complete

--- 02_basic_datatypes/1_numbers/02_05_convert.py
+++ 02_basic_datatypes/1_numbers/02_05_convert.py
@@ -1,12 +1,22 @@
 '''
-
 Demonstrate how to:
-
     1) Convert an int to a float
     2) Convert a float to an int
     3) Perform floor division using a float and an int.
     4) Use two user inputted values to perform multiplication.
 
     Take note of what information is lost when some conversions take place.
-
 '''
+# Convert an int to a float
+num = 123
+print(num, "is of type", type(num))
+
+num = float(num)
+print(num, "is of type", type(num))
+
+
+# Convert a float to an int
+numf = 43.5
+print(numf, "is of type", type(numf))
+numf = int(numf)
+print(numf, "is type of ", type(numf))
