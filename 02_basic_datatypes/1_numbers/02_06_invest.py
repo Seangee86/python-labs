453afeaaae25d92cdb8ae740a0a5f92518362b29
Sean Granier <seanlg624@gmail.com>
2020-09-23 17:19:20 -0500

    02_06 updated

--- 02_basic_datatypes/1_numbers/02_06_invest.py
+++ 02_basic_datatypes/1_numbers/02_06_invest.py
@@ -3,7 +3,10 @@ Take in the following three values from the user:
     - investment amount
     - interest rate in percentage
     - number of years to invest
-
 Print the future values to the console.
-
 '''
+
+invest = int(input('How much are you investing?'))
+irate = float(input('What is the interest rate?'))
+time = int(input('How many years will you be investing?'))
+
