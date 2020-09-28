195c59fb88cc99b3cc49ba2fe7e06e53e2474f9e
Sean Granier <seanlg624@gmail.com>
2020-09-28 15:29:17 -0500

    complete

--- 02_basic_datatypes/1_numbers/02_03_runner.py
+++ 02_basic_datatypes/1_numbers/02_03_runner.py
@@ -2,15 +2,15 @@
 If a runner runs 10 miles in 30 minutes and 30 seconds,
 What is his/her average speed in kilometers per hour? (Tip: 1 mile = 1.6 km)
 '''
-miles = int(input("How many miles did you run?"))
+miles = float(input("How many miles did you run?"))
 
-time = int(float(input(f"How long did it take you to run {miles} miles?")))
+time = float(input(f"How long did it take you to run {miles} miles?"))
   
-mph = time / miles
+mph = miles / time
 
 kilo = round(mph * 1.6, 1)
+#kilo = mph * 1.6
 
 print(f"your average speed was {kilo} Kilometers")
-
 
 
