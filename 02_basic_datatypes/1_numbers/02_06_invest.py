c63c6b57fd941770dd363f9abee1a952342ebe35
Sean Granier <seanlg624@gmail.com>
2020-10-01 05:37:30 -0500

    complete

--- 02_basic_datatypes/1_numbers/02_06_invest.py
+++ 02_basic_datatypes/1_numbers/02_06_invest.py
@@ -7,13 +7,33 @@ Print the future values to the console.
 '''
 
 
+
 invest_amount = int(input("How much are you investing? "))
 
 interest_rate = float(input("what's the interest rate? "))
 
 years_investing = int(input("number of years youre  invest? "))
 
-amount_earned = invest_amount * interest_rate  # $10.00
+# invest_amount = 1000.0
+# interest_rate = .007
+# years_investing = 25.0
 
-total_earnings = amount_earned + invest_amount  # $110
+compound_interest = invest_amount * (interest_rate + 1.0) ** years_investing - invest_amount
+
+print(compound_interest + invest_amount)
+
+# amount_earned = invest_amount * interest_rate  # $10.00
+# total_earnings = amount_earned + invest_amount  # $110
+# Compound Interest=P×(1+r)** t - P
+# where:
+# P=Principal amount
+# r=Annual interest rate
+# t=Number of years interest is applied
+
+
+
+
+
+
+
 
