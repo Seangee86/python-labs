397a5e5b7e82aba22ceaf7143a050cf35312fe5e
Sean Granier <seanlg624@gmail.com>
2020-10-01 05:30:36 -0500

    complete

--- 02_basic_datatypes/2_strings/02_08_occurrence.py
+++ 02_basic_datatypes/2_strings/02_08_occurrence.py
@@ -5,5 +5,21 @@ Find the index of first occurrence of the letter in the string. For example:
 String input: hello world
 Letter input: o
 Result: 4
-
 '''
+
+
+# take in a string of words from the user
+
+words = input("Enter your words: ")
+
+# take in a letter from the user
+
+letter = input("Pick a letter: ")
+
+# Find the index of the first occurrence of the letter in the string
+
+count = words.find(letter)
+
+# print result
+
+print(count)
