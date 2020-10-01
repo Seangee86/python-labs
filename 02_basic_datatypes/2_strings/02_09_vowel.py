6f5279b993d48b39a2a906b7d9df37e1d0cff19d
Sean Granier <seanlg624@gmail.com>
2020-10-01 07:01:18 -0500

    complete

--- 02_basic_datatypes/2_strings/02_09_vowel.py
+++ 02_basic_datatypes/2_strings/02_09_vowel.py
@@ -1,8 +1,29 @@
 '''
 Write a script that prints the total number of vowels that are used in a user-inputted string.
 
-
 CHALLENGE: Can you change the script so that it counts the occurrence of each individual vowel
            in the string and print a count for each of them?
-
 '''
+
+
+# take in a string of words from the user
+string = input("Enter a word or sentence: ")
+
+# what are the vowels in the string
+vowels = 0
+string = string.lower()
+
+# count the vowels in the string
+vowels += string.count("a")
+vowels += string.count("e")
+vowels += string.count("i")
+vowels += string.count("o")
+vowels += string.count("u")
+
+# print the results
+print(vowels)
+
+
+
+
+
