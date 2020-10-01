06ebfbb37599ecd22ef34acc857ab5fd1915a269
Sean Granier <seanlg624@gmail.com>
2020-10-01 14:31:07 -0500

    complete

--- 02_basic_datatypes/2_strings/02_10_most_characters.py
+++ 02_basic_datatypes/2_strings/02_10_most_characters.py
@@ -7,7 +7,41 @@ Example Output:
 5, world
 9, greetings
 
-CHALLENGE: Can you edit to script to print only the string with the most characters? You can look
+CHALLENGE: Can you edit the script to print only the string with the most characters? You can look
            into the topic "Conditionals" to solve this challenge.
-
 '''
+
+
+# string1 = input("first string: ")
+# string2 = input("Second string ")
+# string3 = input("Third string ")
+#
+# print(len(string1), string1)
+# print(len(string2), string2)
+# print(len(string3), string3)
+
+
+# CHALLENGE
+
+# string1 = input("first string: ")
+# string2 = input("Second string ")
+# string3 = input("Third string ")
+#
+# if len(string1) >= len(string2) and len(string1) >= len(string3):
+#     print(string1)
+# elif len(string2) >= len(string1) and len(string2) >= len(string3):
+#     print(string2)
+# else:
+#     print(string3)
+
+
+
+
+
+
+
+
+
+
+
+
