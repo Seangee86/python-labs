cd84d2ad7230493351f8b4cff44b95ac8f275495
Sean Granier <seanlg624@gmail.com>
2020-09-30 20:37:04 -0500

    complete

--- 02_basic_datatypes/2_strings/02_07_replace.py
+++ 02_basic_datatypes/2_strings/02_07_replace.py
@@ -5,6 +5,17 @@ Replace all occurrences of the first letter with the symbol. For example:
 String input: more python programming please
 Symbol input: #
 Result: #ore python progra##ing please
-
 '''
 
+# whats the first step?
+# write input to gather a string of words
+string = input("String input: ")
+
+# next step user input for a symbol
+symbol = input("symbol input: ")
+
+# Replace all occurrences of the first letter with the symbol
+first_letter = string[0]
+replacement = string.replace(first_letter, symbol)
+# print result
+print(replacement)
