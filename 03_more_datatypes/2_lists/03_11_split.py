a00ac8f7dd5ce7d22b75ac289daa9533f30713ae
Sean Granier <seanlg624@gmail.com>
2020-10-06 15:49:24 -0500

    update

--- 03_more_datatypes/2_lists/03_11_split.py
+++ 03_more_datatypes/2_lists/03_11_split.py
@@ -4,5 +4,18 @@ create a list of all the words in the string and print the word with the most
 occurrences.
 '''
 
-string = input("write something")
+# string from user
+#string = input("write something: ")
+string2 = "You are a super super cool cat"     # temp string
+
+# with the split() method, creates a list of each word
+string_list = string2.split()
+
+# create an empty list
+word_count = []
+
+for w in string_list:
+    word_count.append(string_list.count(w))
+
+print(string_list, word_count)
 
