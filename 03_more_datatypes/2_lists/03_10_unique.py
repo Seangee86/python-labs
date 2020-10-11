1a1c6929773464413f58735cd7bed55fbdf4cd7a
Sean Granier <seanlg624@gmail.com>
2020-10-11 13:50:34 -0500

    updated

--- 03_more_datatypes/2_lists/03_10_unique.py
+++ 03_more_datatypes/2_lists/03_10_unique.py
@@ -5,25 +5,11 @@ list_ = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
 unique_list = [55, 'hi', 4, 13]
 
 '''
-list_ = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
-#
-# unique_list = set()
-#
-#
-# print(set(list_))
+list_ = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13, 2]
+
+unique_list = []
 
 # get distinct items
 
 items = list(set(list_))
 
-for i in items:
-    items.count(i)
-if i == 1:
-    print(i)
-
-
-
-
-
-
-print(items)
--- 03_more_datatypes/3_tuples/03_12_list_to_tuple.py
+++ 03_more_datatypes/3_tuples/03_12_list_to_tuple.py
@@ -2,3 +2,8 @@
 Write a script that takes a list and turns it into a tuple.
 
 '''
+
+my_list = [1, 2, 3]
+print(my_list)
+tup = tuple(my_list)
+print(tup)
